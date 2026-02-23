ced93bb1e078c1ad912508a2f346ea74e7c22ac6
ispasdani <ispasdani14@gmail.com>
2026-02-23 21:46:08 +0100

    Add Subheading component to MarketingPage and include arrow icon SVG

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import Subheading from "@/components/marketing-general/subheading";
 import ArticlesFeed from "@/components/marketing-sections/articles-feed";
 import Hero from "@/components/marketing-sections/hero";
 import VideoPresentation from "@/components/marketing-sections/video-presentation/video-presentation";
@@ -9,6 +10,16 @@ function MarketingPage() {
       <VideoPresentation />
       <div className="flex flex-col h-full max-w-[95rem] w-full mx-auto px-4 lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
         <ArticlesFeed />
+      </div>
+
+      <div className="flex flex-col h-full max-w-[95rem] w-full mx-auto px-4 lg:pt-0 sm:pt-4 xs:pt-2 lg:pb-4 md:pb-4 sm:pb-2 xs:pb-2">
+        <Subheading
+          className="text-subheading"
+          url="/features"
+          linkText="All features"
+        >
+          Features
+        </Subheading>
       </div>
     </div>
   );
