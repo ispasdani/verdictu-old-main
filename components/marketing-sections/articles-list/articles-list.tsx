37b79809be84603ac0fb1fcdb1eb7799710abb02
ispasdani <ispasdani14@gmail.com>
2026-03-03 20:14:07 +0100

    Refactor ArticlesList component to use articles data structure and update rendering logic

--- components/marketing-sections/articles-list/articles-list.tsx
+++ components/marketing-sections/articles-list/articles-list.tsx
@@ -2,28 +2,28 @@ import { getArticles } from "@/utils/getArticles";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function ArticlesList() {
-  const data = await getArticles();
+export default function ArticlesList() {
+  const articles = getArticles().flatMap((author) => author.articles);
   return (
     <div className="flex flex-col max-w-[95rem] w-full mx-auto py-12 md:py-48">
-      {data.map((articles, index) => (
-        <div key={articles.id}>
+      {articles.map((article, index) => (
+        <div key={article.slug}>
           <div className="grid grid-cols-1 md:grid-cols-[auto_auto] justify-between md:items-center gap-3 md:gap-0">
             <div className="flex flex-col md:flex-row md:items-center gap-3 md:gap-16">
-              <p className="font-semibold">{articles.episode.slice(7)}</p>
+              <p className="font-semibold">{article.label}</p>
               <Image
                 className="w-[15rem] h-[15rem]"
-                src={articles.img}
-                alt={articles.imgAlt}
+                src={article.img}
+                alt={article.imgAlt}
                 width={240}
                 height={240}
               />
-              <h2 className="heading3-title">{articles.title}</h2>
+              <h2 className="heading3-title">{article.title}</h2>
             </div>
             <div className="flex flex-col md:flex-row md:items-center flex-wrap gap-2">
-              <p>{articles.date}</p>
-              <p>{articles.duration}</p>
-              <Link className="flex gap-2" href={`articles/${articles.slug}`}>
+              <p>{article.date}</p>
+              <p>{article.read}</p>
+              <Link className="flex gap-2" href={`articles/${article.slug}`}>
                 <span className="uppercase font-semibold">Read</span>
                 <img
                   src="/icons/ri_arrow-right-line.svg"
@@ -32,7 +32,7 @@ export default async function ArticlesList() {
               </Link>
             </div>
           </div>
-          {index < data.length - 1 && (
+          {index < articles.length - 1 && (
             <div className="border border-black my-6" />
           )}
         </div>
